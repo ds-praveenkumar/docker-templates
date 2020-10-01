2a4127f5cb7a7c82097348a983a62c4a1471a4f4
ds-praveenkumar <praveenkr2208@gmail.com>
2020-10-02 01:19:00 +0530

    final commit

--- Dockerbuild.sh
+++ Dockerbuild.sh
@@ -1,9 +1,6 @@
-for i in `ls`
+for i in `ls -I "*.txt" -I "*.md" -I "*.sh"`
 do
-    if [  $i != "Dockerbuild.sh" ]
-      then
-       echo "file: $i"
-       chmod +x ./*
-       docker build . -f $i
-    fi
+  echo "file: $i"
+  chmod +x ./*
+  docker build . -f $i --tag my-image-name:$(date +%s)
 done 
