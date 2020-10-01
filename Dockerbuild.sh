84be999c17f23aa41f274e764e6716215cdd37a2
ds-praveenkumar <praveenkr2208@gmail.com>
2020-10-02 00:40:50 +0530

    updated docker build command

--- Dockerbuild.sh
+++ Dockerbuild.sh
@@ -3,6 +3,6 @@ do
     if [  $i != "Dockerbuild.sh" ]
       then
        echo "file: $i"
-       bash -c docker build --tag my-image-name:$(date +%s) -f $i 
+       docker build --tag my-image-name:$(date +%s) -f $i 
     fi
 done 
